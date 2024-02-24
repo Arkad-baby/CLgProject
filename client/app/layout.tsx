dbb99dd0365f9e9eb5c1fa0b8e22e4dc5d2d0719
Kritann <kritanmah2122@gmail.com>
2024-02-24 20:52:45 +0545

    some frontend update

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
-import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,9 +15,8 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    
-          <ClerkProvider>
+  return ( 
+    <ClerkProvider>
     <html lang="en">
       
       <body className={inter.className}>
@@ -27,7 +25,7 @@ export default function RootLayout({
         </body>
      
     </html>
-        </ClerkProvider>
+   </ClerkProvider>
  
   
   );
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,13 +1,23 @@
-import Navbar from '@/components/Navbar'
-import React from 'react'
+"use client"
+import Navbar from '@/components/Navbar';
+import React, { useState, useEffect } from 'react';
+import { useUser } from "@clerk/nextjs";
 
-const page = () => {
+const Page = () => {
+  const [formData, setFormData] = useState({
+    username: '',
+    Email: '',
+    uuid: ''
+  });
+
+  const { isLoaded, isSignedIn, user } = useUser();
+
   return (
-    <div>
-      <Navbar/>
-      <h1 className=' text-center  text-xl md:text-3xl mt-[100px] md:mt-[200px]'>  Welcome to our Canteen's Webpage</h1>
+    <div className="h-[200vh] w-full dark:bg-black text-white bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] ">
+      <Navbar />
+      <h1 className='text-center text-white text-3xl md:text-3xl mt-[20px]'>I am<br /><span className='text-transparent font-bold bg-clip-text bg-gradient-to-r from-purple-500 via-pink-400 to-purple-500'>Hungry</span></h1>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default Page;
