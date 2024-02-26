e033b67d4cc7405c3dbdf069932888c749da924b
Kritann <kritanmah2122@gmail.com>
2024-02-26 11:27:18 +0545

    some changes

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,4 +1,3 @@
-"use client"
 import Navbar from '@/components/Navbar';
 import React, { useState, useEffect } from 'react';
 import { useUser } from "@clerk/nextjs";
@@ -6,11 +5,28 @@ import { useUser } from "@clerk/nextjs";
 const Page = () => {
   const [formData, setFormData] = useState({
     username: '',
-    Email: '',
+    email: '', // Changed 'Email' to 'email' for consistency
     uuid: ''
   });
 
   const { isLoaded, isSignedIn, user } = useUser();
+
+  useEffect(() => {
+    if (isSignedIn && user) {
+      createUser(user);
+    }
+  }, [isSignedIn, user]);
+
+  const createUser = (user:any) => {
+    const { fullName, emailAddresses, id } = user;
+    const email = emailAddresses.length > 0 ? emailAddresses[0].emailAddress : '';
+
+    setFormData({
+      username: fullName,
+      email: email,
+      uuid: id
+    });
+  }
 
   return (
     <div className="h-[200vh] w-full dark:bg-black text-white bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] ">
